242c758f866edda31baf5d52b889ab48621a10d4
sato.hayashi <sato_hayashi_kazuhiro@yahoo.co.jp>
2020-05-14 00:15:32 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   root to: 'tasks#index'
 
+  get 'signup', to: 'users#new'
+  resources :users, only: [:new, :create]
   resources :tasks
 end
