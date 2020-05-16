aaaff51056ec532b796cd6d914d16d9420c70dd7
sato.hayashi <sato_hayashi_kazuhiro@yahoo.co.jp>
2020-05-16 08:24:13 +0000

    for heroku0516

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    flash[:success] = 'タスクを削除しました。'
+    flash[:success] = 'タスクは削除されました。'
     redirect_to root_url
   end
 
