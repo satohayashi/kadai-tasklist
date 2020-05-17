090b2a8a5ec97d42b762b6da2ec2b9254dfc49bc
sato.hayashi <sato_hayashi_kazuhiro@yahoo.co.jp>
2020-05-17 06:44:08 +0000

    post3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,10 +47,6 @@ class TasksController < ApplicationController
 
   private
 
-  def set_task
-    @task = Task.find(params[:id])
-  end
- 
   def task_params
     params.require(:task).permit(:content, :status, :user_id)
   end
